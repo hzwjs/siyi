f3121ec76a7da5f02dfe67304c4ce1816566b505
zhzheng <zzq19940429>
2020-02-29 23:31:35 +0800

    [碰瓷20-03]对错配置信息

---
+++ game-manage/src/main/java/siyi/game/controller/DuiCuoController.java
@@ -0,0 +1,53 @@
+package siyi.game.controller;
+
+import com.alibaba.excel.EasyExcel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import siyi.game.bo.gamelevel.GameLevel;
+import siyi.game.dao.DuiCuoConfigMapper;
+import siyi.game.dao.GamelevelConfigMapper;
+import siyi.game.dao.QuTianziMapper;
+import siyi.game.dao.entity.DuiCuoConfig;
+import siyi.game.dao.entity.GamelevelConfig;
+import siyi.game.manager.excel.read.DuiCuoConfigDataListener;
+import siyi.game.manager.excel.read.GameLevelConfigDataListener;
+import siyi.game.service.gamelevel.GameLevelService;
+import siyi.game.utill.Constants;
+
+import java.io.File;
+
+/**
+ * description: 跟游戏关卡相关的内容，关卡配置信息查询等 <br>
+ * date: 2020/2/25 11:34 <br>
+ * author: hzw <br>
+ * version: 1.0 <br>
+ */
+@Controller
+@RequestMapping(value = "duicuo")
+public class DuiCuoController {
+    private final Logger logger = LoggerFactory.getLogger(DuiCuoController.class);
+
+
+    @Autowired
+    DuiCuoConfigMapper configMapper;
+
+    @RequestMapping(value = "fileAnalysis")
+    @ResponseBody
+    public void fileAnalysis(){
+        try {
+            String fileDir = ClassLoader.getSystemResource("").toURI().getPath();
+            String filePath = fileDir + "biz_config" + File.separator + "Q_duicuo.xlsx";
+            EasyExcel.read(filePath, DuiCuoConfig.class, new DuiCuoConfigDataListener(configMapper)).sheet().doRead();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
