30decf3e8fb14b9c464409eef609db35cf491926
hzwjs <hzw_js@126.com>
2020-02-29 22:41:59 +0800

    公共方法 根据百分比判断是否命中-黄志文

---
+++ game-manage/src/main/java/siyi/game/utill/RandomUtil.java
@@ -0,0 +1,50 @@
+package siyi.game.utill;
+
+import java.util.Random;
+
+/**
+ * 生成随机整数
+ *
+ * @Author hzw
+ */
+public class RandomUtil {
+
+    /**
+     * 随机生成[low,high]之间的随机整数
+     * @param low
+     * @param high
+     * @return
+     */
+    public static int getRandomNumInTwoIntNum(int low, int high) {
+        int randomNum = 0;
+        Random random = new Random();
+        int cha = Math.abs(high - low);
+        if (cha > 0) {
+            randomNum = random.nextInt(high)%(high-low+1) + low;
+        } else if (cha == 0) {
+            randomNum = low;
+        }
+        return randomNum;
+    }
+
+    /**
+     * 根据传入的百分比，判读是否命中
+     * @param percent
+     * @return
+     */
+    public static boolean isHit(String percent) {
+        int percentNum = 0;
+        if (percent.contains("%"))
+            percentNum = Integer.parseInt(percent.substring(0, percent.length() - 1));
+        else percentNum = Integer.parseInt(percent);
+        int randomNum = getRandomNumInTwoIntNum(0, 100);
+        if (randomNum <= percentNum)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(getRandomNumInTwoIntNum(10,12));
+        System.out.println(isHit("50%"));
+    }
+}
