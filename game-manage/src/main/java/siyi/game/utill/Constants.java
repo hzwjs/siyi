225bdcd20543b45cca45d355d2f6981f4625a047
hzwjs <hzw_js@126.com>
2020-02-29 16:48:45 +0800

    全局常量类新建-黄志文

---
+++ game-manage/src/main/java/siyi/game/utill/Constants.java
@@ -0,0 +1,12 @@
+package siyi.game.utill;
+
+/**
+ * 全局常量类
+ */
+public final class Constants {
+
+    private Constants(){};
+
+    public final static String GAME_LEVEL_TYPE_WEN = "wen"; // 关卡类型：文关
+    public final static String GAME_LEVEL_TYPE_WU = "wu"; // 关卡类型：武关
+}
