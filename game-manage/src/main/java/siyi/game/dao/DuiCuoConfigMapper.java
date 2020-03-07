461454c34e76363040585e4e211c12a3257777b6
hzwjs <hzw_js@126.com>
2020-03-07 16:09:21 +0800

    [删除] 删除重复操作类-黄志文

--- game-manage/src/main/java/siyi/game/dao/DuiCuoConfigMapper.java
+++
@@ -1,8 +0,0 @@
-package siyi.game.dao;
-
-import org.springframework.stereotype.Repository;
-import siyi.game.dao.entity.DuiCuoConfig;
-import tk.mybatis.mapper.common.Mapper;
-@Repository
-public interface DuiCuoConfigMapper extends Mapper<DuiCuoConfig> {
-}
--- game-manage/src/main/java/siyi/game/dao/XuanZeConfigMapper.java
+++
@@ -1,7 +0,0 @@
-package siyi.game.dao;
-
-import siyi.game.dao.entity.XuanZeConfig;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface XuanZeConfigMapper extends Mapper<XuanZeConfig> {
-}
