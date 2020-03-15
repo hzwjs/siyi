6b3f8cff9a3d2daa8cbf0b2fd27f0717f6ac4f40
hzwjs <hzw_js@126.com>
2020-03-16 01:40:15 +0800

    [修改] 完善答案矩阵的填充2-黄志文

---
+++ game-manage/src/main/java/siyi/game/bo/gamelevel/CandidateWordTianzi10.java
@@ -0,0 +1,31 @@
+package siyi.game.bo.gamelevel;
+
+import lombok.Data;
+
+/**
+ * 填字题型的待选答案矩阵对象
+ */
+@Data
+public class CandidateWordTianzi10 {
+
+    private String point100;
+
+    private String point101;
+
+    private String point102;
+
+    private String point103;
+
+    private String point104;
+
+    private String point105;
+
+    private String point106;
+
+    private String point107;
+
+    private String point108;
+
+    private String point109;
+
+}
