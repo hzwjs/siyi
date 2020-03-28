502c7da418cccd9e7754313163f4d13a825323d8
hzwjs <hzw_js@126.com>
2020-03-28 20:50:13 +0800

    [修改] 反射操作类增加根据变量名调用set方法赋值-黄志文

--- game-manage/src/main/java/siyi/game/utill/ReflectOperate.java
+++ game-manage/src/main/java/siyi/game/utill/ReflectOperate.java
@@ -1,6 +1,12 @@
 package siyi.game.utill;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -9,20 +15,53 @@ import java.lang.reflect.Method;
 public class ReflectOperate {
 
     /**
+     * The Log.
+     */
+    private final Logger log = LoggerFactory.getLogger(ReflectOperate.class);
+
+    /**
      * 通过属性名调用get方法获取属性值.
      *
-     * @param obj
+     * @param obj  the obj
      * @param name 属性名称
      * @return the get method value
      * @throws Exception the exception
      */
-    public static Object getGetMethodValue(Object obj , String name)throws Exception{
+    public static Object getGetMethodValue(Object obj , String name){
         Method[] m = obj.getClass().getMethods();
         for(int i = 0;i < m.length;i++){
             if(("get"+name).toLowerCase().equals(m[i].getName().toLowerCase())){
-                return m[i].invoke(obj);
+                try {
+                    return m[i].invoke(obj);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
             }
         }
         return null;
+    }
+
+    /**
+     * 通过属性名调用set方法给变量赋值.
+     *
+     * @param obj       the obj
+     * @param fieldName the field name
+     * @param param     the param
+     */
+    public static void setValueByFieldName(Object obj, String fieldName, Object param) {
+        PropertyDescriptor descriptor = null;
+        try {
+            descriptor = new PropertyDescriptor(fieldName, obj.getClass());
+            Method method = descriptor.getWriteMethod();
+            method.invoke(obj, param);
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
