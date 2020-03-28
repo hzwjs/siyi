07d13162225d52facbce4e768e0de867d60ef1ab
hzwjs <hzw_js@126.com>
2020-03-28 21:35:47 +0800

    [修改] 线上环境启动失败的问题-黄志文

--- game-manage/src/main/java/siyi/game/controller/FileAnalysisController.java
+++ game-manage/src/main/java/siyi/game/controller/FileAnalysisController.java
@@ -5,23 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import siyi.game.dao.*;
-import siyi.game.dao.entity.*;
-import siyi.game.manager.excel.read.*;
+import siyi.game.dao.entity.GamelevelConfig;
+import siyi.game.dao.entity.IdiomWrong;
+import siyi.game.dao.entity.QuTianzi;
+import siyi.game.dao.entity.Tianzi4;
+import siyi.game.manager.excel.read.GameLevelConfigDataListener;
+import siyi.game.manager.excel.read.QuDuicuo2DataListener;
+import siyi.game.manager.excel.read.QuTianzi4DataListener;
+import siyi.game.manager.excel.read.QuTianziDataListener;
 
 import java.io.File;
-import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("idiom/file/analysis")
 public class FileAnalysisController {
-    private static String fileDir = "";
-    static {
-        try {
-            fileDir = ClassLoader.getSystemResource("").toURI().getPath();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Autowired
     GamelevelConfigMapper gamelevelConfigMapper;
@@ -37,6 +34,7 @@ public class FileAnalysisController {
     @RequestMapping("guanqiaConfig")
     public void guanqiaConfigAnalysis() {
         try {
+            String fileDir = ClassLoader.getSystemResource("").toURI().getPath();
             String filePath = fileDir + "biz_config" + File.separator + "guanqia20200322.xlsx";
             EasyExcel.read(filePath, GamelevelConfig.class, new GameLevelConfigDataListener(gamelevelConfigMapper)).sheet().doRead();
         } catch (Exception e) {
@@ -47,6 +45,7 @@ public class FileAnalysisController {
     @RequestMapping("tianzi")
     public void tianziAnalysis() {
         try {
+            String fileDir = ClassLoader.getSystemResource("").toURI().getPath();
             String filePath = fileDir + "biz_config" + File.separator + "Q_tianzi.xlsx";
             EasyExcel.read(filePath, QuTianzi.class, new QuTianziDataListener(quTianziMapper)).sheet().doRead();
         } catch (Exception e) {
@@ -57,6 +56,7 @@ public class FileAnalysisController {
     @RequestMapping("duicuo2")
     public void duicuo2Analysis() {
         try {
+            String fileDir = ClassLoader.getSystemResource("").toURI().getPath();
             String filePath = fileDir + "biz_config" + File.separator + "Q_duicuo2.xlsx";
             EasyExcel.read(filePath, IdiomWrong.class, new QuDuicuo2DataListener(idiomWrongMapper, idiomMapper)).sheet().doRead();
         } catch (Exception e) {
@@ -67,6 +67,7 @@ public class FileAnalysisController {
     @RequestMapping("tianzi4")
     public void tianzi4Analysis() {
         try {
+            String fileDir = ClassLoader.getSystemResource("").toURI().getPath();
             String filePath = fileDir + "biz_config" + File.separator + "Q_tianzi4.xlsx";
             EasyExcel.read(filePath, Tianzi4.class, new QuTianzi4DataListener(tianzi4Mapper)).sheet().doRead();
         } catch (Exception e) {
