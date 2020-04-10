802a21df83b251ba60ee5ba10177492317f5e933
zhengzhiqiang <zhiqiang.zheng3@outlook.com>
2020-04-10 09:43:26 +0800

    修改Mapper配置注解

--- game-manage/src/main/java/siyi/game/dao/PlayerWxInfoMapper.java
+++ game-manage/src/main/java/siyi/game/dao/PlayerWxInfoMapper.java
@@ -1,7 +1,9 @@
 package siyi.game.dao;
 
+import org.springframework.stereotype.Repository;
 import siyi.game.dao.entity.PlayerWxInfo;
 import tk.mybatis.mapper.common.Mapper;
 
+@Repository
 public interface PlayerWxInfoMapper extends Mapper<PlayerWxInfo> {
 }
