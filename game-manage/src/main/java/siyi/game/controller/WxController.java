b98d2a824ce27c9779a260539c2dd1da39d8d599
hzwjs <hzw_js@126.com>
2020-04-11 23:04:25 +0800

    [新增] 关卡信息上送微信托管2-黄志文

--- game-manage/src/main/java/siyi/game/controller/WxController.java
+++ game-manage/src/main/java/siyi/game/controller/WxController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import siyi.game.dao.PlayerMapper;
+import siyi.game.dao.entity.Player;
 import siyi.game.service.wx.WxService;
 import siyi.game.utill.CacheClass;
 
@@ -21,6 +23,8 @@ public class WxController extends BaseController{
     @Autowired
     WxService wxService;
     @Autowired
+    PlayerMapper playerMapper;
+    @Autowired
     private RestTemplate restTemplate;
 
     private static final String SIGNTYPE = "HMACSHA256";
@@ -28,18 +32,27 @@ public class WxController extends BaseController{
 
     @RequestMapping("setUserStorage")
     @ResponseBody
-    public void setUserStorage(String openid, String level, String sessionKey) {
+    public void setUserStorage(String playerId, String level, String sessionKey) {
         Map data = new HashMap();
         data.put("level", level);
-        String signature = wxService.generateSignature(data, sessionKey, SIGNTYPE);
-        String accessToken = CacheClass.getCache("accessToken");
-        String url = "https://api.weixin.qq.com/wxa/set_user_storage?access_token="+accessToken+"&signature="+signature+"&openid="+openid+"&sig_method="+SIGNTYPE;
-        Map response = restTemplate.postForObject(url, data, Map.class);
-        if (SUCCESS_CODE.equals(response.get("errcode"))) {
-            getSuccessResult(new HashMap<>());
+
+        Player player = new Player();
+        player.setPlayerId(playerId);
+        player = playerMapper.selectOne(player);
+        if (player != null) {
+            String signature = wxService.generateSignature(data, sessionKey, SIGNTYPE);
+            String accessToken = CacheClass.getCache("accessToken");
+            String url = "https://api.weixin.qq.com/wxa/set_user_storage?access_token=" + accessToken + "&signature=" + signature + "&openid=" + player.getPlatformId() + "&sig_method=" + SIGNTYPE;
+            Map response = restTemplate.postForObject(url, data, Map.class);
+            if (SUCCESS_CODE.equals(response.get("errcode"))) {
+                getSuccessResult(new HashMap<>());
+            } else {
+                getFailResult(new HashMap<>(), "=== 用户的关卡信息上送失败 ===");
+            }
         } else {
-            getFailResult(new HashMap<>(), "=== 用户的关卡信息上送失败 ===");
+            getFailResult(new HashMap<>(), "=== playerId非法 ===");
         }
+
     }
 
 }
