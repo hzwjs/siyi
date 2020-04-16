882dfb4845ac4c1eafa6175cafabcd51048882be
hzwjs <hzw_js@126.com>
2020-04-16 22:19:50 +0800

    [新增] 新增抽奖的定时任务2-黄志文

--- game-manage/src/main/java/siyi/game/manager/scheduled/FunctionScheduled.java
+++ game-manage/src/main/java/siyi/game/manager/scheduled/FunctionScheduled.java
@@ -1,6 +1,5 @@
 package siyi.game.manager.scheduled;
 
-import com.github.pagehelper.PageHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,13 +16,7 @@ public class FunctionScheduled {
      * 每天12点执行，给玩家增加一次抽奖机会
      */
     @Scheduled(cron = "0 59 23 * * *")
-    private void getAccessToken() {
-//        int size = 2;
-//        int total = playerMapper.selectCount(null);
-//        int pages = total/2 + (total % 2 == 0?0:1);
-//        for (int i = 1; i <= pages; i++) {
-//
-//        }
+    private void addLotteryNum() {
         playerMapper.updateLotteryNumBatch();
     }
 }
