81a9066bd6b571c8f0e5c907f6f4fa90db8b38ca
hzwjs <hzw_js@126.com>
2020-04-19 21:03:36 +0800

    [新增] 新增道具使用入口-黄志文

---
+++ game-manage/src/main/java/siyi/game/controller/ItemUseController.java
@@ -0,0 +1,12 @@
+package siyi.game.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 道具使用方法
+ */
+@Controller
+@RequestMapping("idiom/itemuse")
+public class ItemUseController {
+}
