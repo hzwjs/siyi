5eda50fbcbb18e4f7149cfe40ddc1b8f5a3ad3fc
hzwjs <hzw_js@126.com>
2020-04-25 23:08:41 +0800

    [修改] 体力消耗、计算3-黄志文

--- game-manage/src/main/java/siyi/game/dao/PhysicalPowerMapper.java
+++ game-manage/src/main/java/siyi/game/dao/PhysicalPowerMapper.java
@@ -1,7 +1,8 @@
 package siyi.game.dao;
 
+import org.springframework.stereotype.Component;
 import siyi.game.dao.entity.PhysicalPower;
 import tk.mybatis.mapper.common.Mapper;
-
+@Component
 public interface PhysicalPowerMapper extends Mapper<PhysicalPower> {
 }
